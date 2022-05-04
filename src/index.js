94a4768d3503dedec6f74d02ae1d9f6ddc8e8afb
kwonjihyeon2 <kjh2868@gmail.com>
2022-05-04 21:44:14 +0900

    redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const add = document.getElementById("Add")
+const minus = document.getElementById("Minus")
+const number = document.querySelector("span")
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//string 값을 변수로 설정하면 오류를 js에서 쉽게 추적 가능
+const ADD = "ADD"
+const MINUS = "MINUS"
+
+const countModifier = (state = 0, action) => {
+   //데이터를 바꾸는 함수
+  // if(action.type === "ADD"){
+  //   return state + 1
+  // } else if(action.type === "MINUS"){
+  //   return state - 1
+  // } else {
+  //   return state
+  // }
+
+  //if 대신 switch, if : case/ else : default
+  switch(action.type){
+    case ADD : 
+      return state + 1
+    case MINUS :
+      return state - 1
+    default :
+      return state;
+  }
+}
+const store = createStore(countModifier)
+
+const handleAdd = () => {
+  //state를 바꿔주기 위해 action과 dispatch를 연결해주어야함.
+  //dispatch는 object(=>{type: ...})로 작성해주어야함
+  store.dispatch({type:ADD})
+}
+
+const handleMinus = () => {
+  store.dispatch({type:MINUS})
+}
+
+number.innerText = 0
+
+const onChange = () =>{
+  //HTML 변경 함수
+  number.innerText = store.getState()
+}
+//store에 영향을 주기 위해서 함수를 subscribe
+store.subscribe(onChange)
+
+add.addEventListener("click", handleAdd)
+minus.addEventListener("click", handleMinus)
